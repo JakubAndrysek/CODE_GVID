2e996cfccaa9f9495e507ba9a4eaa087f1df47ac
Jakub Andrysek <jakub.andrysek03@gmail.com>
2019-09-18 22:27:45 +0200

    Vytvoreni Switch vetvi v Ciselne_soustavy

--- Domaci/Ciselne_soustavy/main.c
+++ Domaci/Ciselne_soustavy/main.c
@@ -1,8 +1,110 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define showNK false
+
+int From=4, To=1;
+
+
 int main()
 {
-    printf("Ahoj, ja jsem inteligentni prevodnik ciselnych soustav NUKON\n");
+    printf("Ahoj, ja jsem inteligentni prevodnik ciselnych soustav NUKON\n\n");
+
+    while(1){
+        //Z jake soustavy
+        printf("Z jake soustavy chces prevadet?\n");
+        printf("1-Dvojkove; 2-Ctyrkove; 3-Osmickove; 4-Desitkove; 5-Sestnactkove\n");
+        //scanf("%d", &From);
+        printf("Cislo: %d \n",From);
+        //Do jake soustavy
+        printf("Do jake soustavy chces prevadet?\n");
+        printf("1-Dvojkove; 2-Ctyrkove; 3-Osmickove; 4-Desitkove; 5-Sestnactkove\n");
+
+        //scanf("%d", &To);
+        printf("Cislo: %d \n",To);
+
+        switch(From)
+        {
+            case 1:
+                switch(To)
+                {
+                    case 1: //Dvojkova
+                        break;
+                    case 2: //Ctyrkova
+                        break;
+                    case 3: //osmickova
+                        break;
+                    case 4: //desitkova
+                        break;
+                    case 5: //sestnactkova
+                        break;
+                }
+
+            case 2:
+               switch(To)
+               {
+                    case 1: //Dvojkova
+                        break;
+                    case 2: //Ctyrkova
+                        break;
+                    case 3: //osmickova
+                        break;
+                    case 4: //desitkova
+                        break;
+                    case 5: //sestnactkova
+                        break;
+                }
+            case 3:
+               switch(To)
+               {
+                    case 1: //Dvojkova
+                        break;
+                    case 2: //Ctyrkova
+                        break;
+                    case 3: //osmickova
+                        break;
+                    case 4: //desitkova
+                        break;
+                    case 5: //sestnactkova
+                        break;
+                }
+            case 4:
+               switch(To)
+               {
+                    case 1: //Dvojkova
+                        break;
+                    case 2: //Ctyrkova
+                        break;
+                    case 3: //osmickova
+                        break;
+                    case 4: //desitkova
+                        break;
+                    case 5: //sestnactkova
+                        break;
+                }
+            case 5:
+               switch(To)
+               {
+                    case 1: //Dvojkova
+                        printf("Z 2 do 4");
+                        break;
+                    case 2: //Ctyrkova
+                        break;
+                    case 3: //osmickova
+                        break;
+                    case 4: //desitkova
+                        break;
+                    case 5: //sestnactkova
+                        break;
+                }
+
+
+
+        }
+
+        break;
+    }
+
     return 0;
 }
