cf2f5323f53247978ddf6be159af97e01c0968c9
Jakub Andrysek <jakub.andrysek03@gmail.com>
2020-01-19 22:30:21 +0100

    Oprava nedodelanych projektu

--- Skolni/Cviceni5/program/main.c
+++ Skolni/Cviceni5/program/main.c
@@ -1,124 +1,4 @@
-/*
- * Projekt: Program5
- * Autor:   Jakub Andrysek
- * Datum:   2019-12-9
- */
-/*
-#include <stdio.h>
-#include <stdlib.h>
-// #include <string.h>  // pro praci s textovymi retezci
-// #include <stdbool.h> // pro praci s typem bool a konstantami true a false
- #include <ctype.h>   // isalpha, isspace, islower, isupper, ...
-// #include <math.h>    // funkce z matematicke knihovny
-// #include <float.h>   // konstanty pro racionální typy DBL_MAX, DBL_DIG, ...
-// #include <limits.h>  // konstanty pro celočíselné typy INT_MAX, INT_MIN, ...
 
-
-// zkratka
-typedef unsigned int uint;
-
-void ukol1()
-{
-  int a, poc = 0;
-  while(((a = getchar()) != '\n'))
-  {
-    putchar(a);
-    poc++;
-  }
-  printf("\nNacetlo se %d znaku\n", poc);
-}
-
-void ukol1B()
-{
-  int a, poc = 0;
-  while(((a = getchar()) != '\n'))
-  {
-    a = toupper(a); //Prevod malych pismen na velka
-    putchar(a);
-    poc++;
-  }
-  printf("\nNacetlo se %d znaku\n", poc);
-}
-
-
-void ukol2A()
-{
-  int a, posun = 2;
-  while(((a = getchar()) != '\n' && a != EOF))
-  {
-    putchar(a+posun);
-  }
-}
-
-
-void cesar(int posun)
-{
-  int a;
-  while(((a = getchar()) != '\n' && a != EOF))
-  {
-    putchar(a+posun);
-  }
-}
-
-void ukol2B()
-{
-  int a, posun;
-  printf("Zadej posun:");
-  scanf("%d ", &posun);
-
-  cesar(posun);
-}
-
-void ukol3A()
-{
-  int cislo;
-  scanf("%d", &cislo);
-  while(cislo != 0)
-  {
-    putchar(('0'+(cislo%10)));
-    cislo/=10;
-  }
-}
-
-
-void ukol3B()
-{
-  int cislo, cifra = 1, vystup = 0;
-  printf("Zadej cislo, ktere mam prevratit:");
-  scanf("%d", &cislo);
-  while(cislo != 0)
-  {
-    vystup = vystup*10+(cislo%10);
-    cislo/=10;
-  }
-  printf("\nPrevracene cislo:%d\n",vystup);
-}
-
-void ukol4A()
-{
-  int pocet, vstup;
-  scanf("%d", pocet);
-  for(int i = 0; i < pocet; i++)
-  {
-    scanf()
-    if
-  }
-}
-
-
-
-int main()
-{
-  //ukol1();
-  //ukol1B();
-  //ukol2A();
-  //ukol2B();
-  //ukol3A();
-  ukol3B();
-  return 0;
-}
-
-*/
 /*
  * Projekt: Program5
  * Autor:   Jakub Andrysek
@@ -233,26 +113,6 @@ void ukol5A()
 }
 
 
-void ukol5B()
-{
-  int pocet, vstup, pocitadlo = 0, pozice[100], pocPoz = 0;
-  printf("Zadej pocet nacitanych cisel\n", pocet);
-  scanf("%d", &pocet);
-  printf("Zadej %d cislel\n", pocet);
-  for(int i = 0; i < pocet; i++)
-  {
-  	scanf("%d", &vstup);
-  	if(vstup == 7)
-  	{
-  		pocitadlo++;
-  		pozice[pocPoz] =
-  	}
-  }
-  printf("Pocet vyskytu cisla 7 je:%d", pocitadlo);
-}
-
-
-
 int main()
 {
   //ukol1();
@@ -261,7 +121,6 @@ int main()
   //ukol2B();
   //ukol3();
   //ukol4();
-  ukol5A();
-  //ukol5B();
+  //ukol5A();
   return 0;
 }
