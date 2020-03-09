6314d5dbd6aeb26d9a64fd84637192b1e99b1c3e
„JakubAndrysek“ <jakub.andrysek03@gmail.com>
2020-03-09 15:06:18 +0100

    15 hodina

--- Skolni/Cviceni14/Program14/main.c
+++ Skolni/Cviceni14/Program14/main.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>  // pro praci s textovymi retezci
+#include <string.h>  // pro praci s textovymi retezci
 // #include <stdbool.h> // pro praci s typem bool a konstantami true a false
 // #include <ctype.h>   // isalpha, isspace, islower, isupper, ...
 // #include <math.h>    // funkce z matematicke knihovny
@@ -44,17 +44,11 @@ int delkaRetezce(char retez[])
   return delkaTextu;
 }
 
-void zvojeni(char retez[])
+void zdvojeni(char retez[])
 {
-  int delkaTextu;
-
   scanf("%19s", retez);
-  for(delkaTextu = 0; retez[delkaTextu] != '\0'; delkaTextu++)
-  {
-    ;
-  }
 
-  for(int x = 0; x < delkaTextu; x++)
+  for(int x = 0; x < delkaRetezce(retez); x++)
   {
     printf("%c%c", retez[x], retez[x]);
   }
@@ -63,12 +57,71 @@ void zvojeni(char retez[])
 void ukol1()
 {
   char retez[20];
-  zvojeni(retez);
+  zdvojeni(retez);
+}
+
+void otocVypis(char retez[])
+{
+  for(int i = delkaRetezce(retez)-1; i >= 0; i--)
+  {
+    printf("%c", retez[i]);
+  }
+}
+
+void otocDoRetezce(char vstup[])
+{
+  int delka = strlen(vstup);
+
+  for(int i = 0; i < delka/2; ++i)
+  {
+    char pom = vstup[i];
+    vstup[i] = vstup[delka-i-1];
+    vstup[delka-i-1] = pom;
+  }
+
+}
+
+
+void ukol2()
+{
+  char retez[21];
+  char retezVystup[21];
+
+  scanf("%20s", retez);
+  printf("%s", retez);
+  printf("\nOtoc: ");
+  otocVypis(retez);
+
+  printf("\nOtocRetez: ");
+  otocDoRetezce(retez);
+  puts(retez);
+}
+
+
+void naplnZnakem(char retez[], int velikost, char znak)
+{
+  for(int i = 0; i < velikost; i++)
+  {
+    retez[i] = znak;
+  }
+  retez[velikost] = '\0';
+}
+
+
+void ukol3()
+{
+  char retez[101] = ""; //prazdny retezec
+  char znak = 'k';
+  naplnZnakem(retez, 8, znak);
+  printf("V poli je %s", retez);
+
 }
 
 
 int main()
 {
-  ukol1();
+  //ukol1();
+  //ukol2();
+  ukol3();
   return 0;
 }
