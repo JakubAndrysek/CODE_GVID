d0953c27b0751c3883c9292aa36e40baeccee65e
Jakub Andrysek <jakub.andrysek03@gmail.com>
2022-11-29 21:56:54 +0100

    Otaceni

--- bash/seminar/otoceni-souboru/oteoceni-pozpatku.sh
+++ bash/seminar/otoceni-souboru/oteoceni-pozpatku.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
-filename='oteoceni-pozpatku.txt'
-n=1
-while read line; do
-# reading each line
-tac <<< "$line"
-done < $filename | tac 
+# #!/bin/bash
+
+# echo stream input
+# cat
+
+# Solution 1
+# echo stream input reversed
+rev | tac
+
+
+# Solution 2
+# read stream by lines and reverse them
+# tac | while read line; do
+# 	echo $line | rev
+# done
+
+
+# Solution 3
+# read stream by lines and print line number
+# tac | while read line; do
+# 	echo $line | rev
+# done | nl
